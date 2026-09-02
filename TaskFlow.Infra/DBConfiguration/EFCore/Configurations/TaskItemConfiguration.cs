a5f01e68bf9120926dfad7da9ee66d117d59f8dd
vitor gaefke <vitorgaefke@gmail.com>
2026-09-02 16:56:18 -0300

    separa modelbuilder por entidade

---
+++ TaskFlow.Infra/DBConfiguration/EFCore/Configurations/TaskItemConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TaskFlow.Domain.Entities;
+
+
+namespace TaskFlow.Infra.DBConfiguration.EFCore.Configurations
+{
+    public class TaskItemConfiguration : IEntityTypeConfiguration<TaskItem>
+    {
+        public void Configure(EntityTypeBuilder<TaskItem> entity)
+        {
+            entity.HasOne(task => task.Status)
+                    .WithMany() // Um Status pode ter muitos TaskItems
+                    .HasForeignKey(task => task.StatusId) // Chave estrangeira em TaskItem
+                    .OnDelete(DeleteBehavior.Restrict); // Evita exclusão em cascata
+        }
+    }
+}
